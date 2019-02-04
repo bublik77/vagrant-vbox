6dec18eb80a5e2d3e3050bb670295ff4e0c20e78
bublik77 <andriy.bobrovych@gmail.com>
2019-02-04 13:00:25 +0200

    clean mysql-setup script

--- provision/mysql-setup.sh
+++ provision/mysql-setup.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 GLOBAL_PATH="/home/vagrant"
-#PASSWD=$(date +%s | sha256sum | base64 | head -c 32 ; echo)
-#yum -y update
 
 if [[ ! -f $(find /usr/bin -type f -name wget) ]]
 then
@@ -17,11 +15,8 @@ if [[ $? -eq 0 ]]
 then
 	yum -y install mysql-server
 	systemctl start mysqld
-	#echo $PASSWD > $GLOBAL_PATH/sql_passwd.txt
-	#echo -e "\033[0;32mYou MySQL password save in sql_passwd.txt in your home directory\033[0m\n"
-	#mysqladmin -u root password $PASSWD
 	grep 'temporary password' /var/log/mysqld.log | cut -d ':' -f 4 | tr -d " " > sql_passwd.txt
-	echo -e "\033[0;32mYou TEMPORARY MySQL password save in sql_passwd.txt in your home directory, please do not forget cange it\033[0m\n"
+	echo -e "\033[0;31mYou TEMPORARY MySQL password save in sql_passwd.txt in your home directory, please do not forget cange it\033[0m\n"
 else 
 	echo -e "\033[0;31mSomething went wrong....\033[0m\n"
 fi
